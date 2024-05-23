f19a2472ec99cb9a88c75fe63d8a7280a7a7e810
LAPTOP-5SVGVIPE\eilat <2495072@edu.vaniercollege.qc.ca>
2024-05-23 14:54:59 -0400

    Select statement added

--- assignment1.sql
+++ assignment1.sql
@@ -10,6 +10,9 @@ CREATE TABLE Clients (
 INSERT INTO Clients (cId, firstName, lastName, address) VALUES (1, 'Tom', 'Harper', '77st. Joliette, Montreal');
 INSERT INTO Clients (cId, firstName, lastName, address) VALUES (2, 'Henry', 'Cooper', '45st. Monk, Montreal');
 
+SELECT * FROM Clients
+WHERE address LIKE '%real%';
+
 SELECT cid, CONCAT_WS(' ', firstName, lastName) AS fullName FROM Clients;
 SELECT cid, REPLACE(address, 'Montreal', 'Quebec') AS newAddress FROM Clients;
 SELECT cid, LENGTH(address) AS LengthOfAddress FROM Clients;
